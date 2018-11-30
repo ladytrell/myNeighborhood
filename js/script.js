3cafe47a97a57cbc98a8908837d1f7fcd40dc68e
Antrell Kent <antrellkent@gmail.com>
2018-11-29 23:30:18 -0500

    Added functionality to locationList click for infoWindow

--- js/script.js
+++ js/script.js
@@ -4,37 +4,45 @@ var initialLocations = [
         position: {lat: 35.9625102, lng: -78.90066589999999},
         title: 'Southside Church of Christ',
         address: '800 Elmira Ave, Durham, NC 27707, USA',
-        place_id: 'ChIJF6ZUnrTlrIkRWQCZsktyJJE'
+        place_id: 'ChIJF6ZUnrTlrIkRWQCZsktyJJE',
+        markerId: null
     },
     {
         title: 'Hillandale Golf course',
         position: {lat: 36.0240281, lng: -78.9353211},
         address: '1600 Hillandale Rd, Durham, NC 27705, USA',
-        place_id: 'ChIJGZJ8zlvhrIkRoyKabDyTBlQ'
+        place_id: 'ChIJGZJ8zlvhrIkRoyKabDyTBlQ',
+        markerId: null
     },
     {        
         title: 'Shuckin\' Shack Oyster Bar',
         position: {lat: 36.0076623, lng: -78.9267354},
         address: 'Erwin Square, 2200 West Main Street A-140, Durham, NC 27705, United States',
-        place_id: 'ChIJe4ufXQDkrIkRZuAeC8k1rds'
+        place_id: 'ChIJe4ufXQDkrIkRZuAeC8k1rds',
+        markerId: null
     },
     {
         title: 'University BP Service Center',
         position:  {lat: 35.9862294, lng: -78.90885609999999},
         address:  '1101 University Dr, Durham, NC 27707, USA',
-        place_id: 'ChIJgUJ61T_krIkRYZW4_3f6MLY'
+        place_id: 'ChIJgUJ61T_krIkRYZW4_3f6MLY',
+        markerId: null
     },
     {
         title: 'Hayti Heritage Center', 
         position: {lat: 35.9856855, lng: -78.89794499999999},
         address:  '804 Old Fayetteville St, Durham, NC 27701, USA',
-        place_id: 'ChIJC9-j02nkrIkR33TORYex9q0'
+        place_id: 'ChIJC9-j02nkrIkR33TORYex9q0',
+        markerId: null
     }
 ]
 
 
 var map;
-
+var markers = [];
+var largeInfowindow;
+var markerId = 0;
+    
 function ViewModel(){
     
     var self = this;  //the instance or pointer of the ViewModel
@@ -44,6 +52,28 @@ function ViewModel(){
     initialLocations.forEach(function(locationItem){
         self.locationList.push( locationItem );
     });
+    
+    self.setLocationMarkerID = function( locationItem,locationId){
+        this.locationList()[locationItem].markerId = locationId;
+    };
+    
+    // Function to link locationList to the markers.  Try matching the positions
+    //this inside the function is the locationList{} due to the with binding in the html
+    this.showInfoWindow = function(){
+        console.log(this.title);
+        console.log(this.position);
+        console.log(this.markerId);
+        for (var i = 0; i < markers.length; i++) {
+        console.log(markers[i].title);
+        console.log(markers[i].position);
+        console.log(markers[i].id);
+            if(this.markerId == markers[i].id){
+            //if(this.position.lat == markers[i].position.lat && this.position.lng == markers[i].position.lng){
+                populateInfoWindow(markers[i], largeInfowindow);
+                break;
+            }
+        }
+    };
 }
 var viewModel = new ViewModel();
 ko.applyBindings(new ViewModel());
@@ -52,7 +82,7 @@ ko.applyBindings(new ViewModel());
 function initMap() {
     var self = this;  //the instance or pointer of map
     
-    markers = []; 
+ 
     // Constructor creates a new map - Centered at Durham, NC.
     map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 35.9940329, lng: -78.898619},
@@ -61,10 +91,15 @@ function initMap() {
       mapTypeControl: false
     });
     
+    createMarkers();
+}
+
+function createMarkers(){    
     // Udacity Projectcode3windowshoppingpart1
     // The following group uses the locationList array to create an array of markers on initialize.
     
-    var largeInfowindow = new google.maps.InfoWindow();
+    //var largeInfowindow = new google.maps.InfoWindow();
+    largeInfowindow = new google.maps.InfoWindow();
         
     for (var i = 0; i < viewModel.locationList().length; i++) {
         // Get the position from the location array.
@@ -76,8 +111,10 @@ function initMap() {
             position: position,
             title: title,
             animation: google.maps.Animation.DROP,
-            id: i
+            id: markerId
         });
+        viewModel.setLocationMarkerID(i,markerId);
+        markerId++;
         // Push the marker to our array of markers.
         markers.push(marker);
         // Create an onclick event to open an infowindow at each marker.
