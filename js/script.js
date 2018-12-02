69247fa935da945b99b7cfb0c78d758ba7ae7fa5
Antrell Kent <antrellkent@gmail.com>
2018-12-01 23:25:26 -0500

    Correct separation of concerns

--- js/script.js
+++ js/script.js
@@ -37,11 +37,11 @@ var initialLocations = [
     }
 ]
 
-
+/*
 var map;
 var markers = [];
 var largeInfowindow;
-var markerId = 0;
+var markerId = 0; */
     
 function ViewModel(){
     
@@ -63,80 +63,136 @@ function ViewModel(){
         console.log(this.title);
         console.log(this.position);
         console.log(this.markerId);
-        for (var i = 0; i < markers.length; i++) {
-        console.log(markers[i].title);
-        console.log(markers[i].position);
-        console.log(markers[i].id);
-            if(this.markerId == markers[i].id){
+        for (var i = 0; i < mapView.markers.length; i++) {
+        console.log(mapView.markers[i].title);
+        console.log(mapView.markers[i].position);
+        console.log(mapView.markers[i].id);
+            if(this.markerId == mapView.markers[i].id){
             //if(this.position.lat == markers[i].position.lat && this.position.lng == markers[i].position.lng){
-                populateInfoWindow(markers[i], largeInfowindow);
+                mapView.populateInfoWindow(mapView.markers[i], mapView.largeInfowindow);
                 break;
             }
         }
     };
+    /*
+    // Udacity Project_Code_10_DisplayRoutesDirectionsService.html
+    document.getElementById('search-within-time').addEventListener('click', function() {
+        searchWithinTime();
+    });
+    */
 }
 var viewModel = new ViewModel();
 ko.applyBindings(new ViewModel());
 
-// Initialize the map
-function initMap() {
-    var self = this;  //the instance or pointer of map
+function MapView(){
     
- 
-    // Constructor creates a new map - Centered at Durham, NC.
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 35.9940329, lng: -78.898619},
-      zoom: 13,
-      //styles: styles,
-      mapTypeControl: false
-    });
-    
-    createMarkers();
-}
+    var self = this;  //the instance or pointer of the MapView
+    this.map;
+    this.markers = [];
+    this.largeInfowindow;
+    this.markerId = 0;
 
-function createMarkers(){    
-    // Udacity Projectcode3windowshoppingpart1
-    // The following group uses the locationList array to create an array of markers on initialize.
-    
-    //var largeInfowindow = new google.maps.InfoWindow();
-    largeInfowindow = new google.maps.InfoWindow();
+    // Initialize the map
+    this.initMap = function() {
+     
+        // Constructor creates a new map - Centered at Durham, NC.
+        self.map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 35.9940329, lng: -78.898619},
+          zoom: 13,
+          //styles: styles,
+          mapTypeControl: false
+        });
         
-    for (var i = 0; i < viewModel.locationList().length; i++) {
-        // Get the position from the location array.
-        var position = viewModel.locationList()[i].position;
-        var title = viewModel.locationList()[i].title;
-        // Create a marker per location, and put into markers array.
-        var marker = new google.maps.Marker({
-            map: map,
-            position: position,
-            title: title,
-            animation: google.maps.Animation.DROP,
-            id: markerId
-        });
-        viewModel.setLocationMarkerID(i,markerId);
-        markerId++;
-        // Push the marker to our array of markers.
-        markers.push(marker);
-        // Create an onclick event to open an infowindow at each marker.
-        marker.addListener('click', function() {
-            populateInfoWindow(this, largeInfowindow);
-        });
-    }
-}  
+        this.createMarkers();
+    };
 
-// Udacity Projectcode3windowshoppingpart1
-// This function populates the infowindow when the marker is clicked. We'll only allow
-// one infowindow which will open at the marker that is clicked, and populate based
-// on that markers position.
-function populateInfoWindow(marker, infowindow) {
-    // Check to make sure the infowindow is not already opened on this marker.
-    if (infowindow.marker != marker) {
-        infowindow.marker = marker;
-        infowindow.setContent('<div>' + marker.title + '</div>');
-        infowindow.open(map, marker);
-        // Make sure the marker property is cleared if the infowindow is closed.
-        infowindow.addListener('closeclick',function(){
-            infowindow.setMarker = null;
-        });
-    }
+    this.createMarkers = function() {    
+        // Udacity Projectcode3windowshoppingpart1
+        // The following group uses the locationList array to create an array of markers on initialize.
+        
+        //var largeInfowindow = new google.maps.InfoWindow();
+        this.largeInfowindow = new google.maps.InfoWindow();
+            
+        for (var i = 0; i < viewModel.locationList().length; i++) {
+            // Get the position from the location array.
+            var position = viewModel.locationList()[i].position;
+            var title = viewModel.locationList()[i].title;
+            // Create a marker per location, and put into markers array.
+            var marker = new google.maps.Marker({
+                map: self.map,
+                position: position,
+                title: title,
+                animation: google.maps.Animation.DROP,
+                id: self.markerId
+            });
+            viewModel.setLocationMarkerID(i,self.markerId);
+            self.markerId++;
+            // Push the marker to our array of markers.
+            this.markers.push(marker);
+            // Create an onclick event to open an infowindow at each marker.
+            marker.addListener('click', function() {
+                populateInfoWindow(this, largeInfowindow);
+            });
+        }
+    };
+
+    // Udacity Projectcode3windowshoppingpart1
+    // This function populates the infowindow when the marker is clicked. We'll only allow
+    // one infowindow which will open at the marker that is clicked, and populate based
+    // on that markers position.
+    this.populateInfoWindow = function (marker, infowindow) {
+        // Check to make sure the infowindow is not already opened on this marker.
+        if (infowindow.marker != marker) {
+            infowindow.marker = marker;
+            infowindow.setContent('<div>' + marker.title + '</div>');
+            infowindow.open(map, marker);
+            // Make sure the marker property is cleared if the infowindow is closed.
+            infowindow.addListener('closeclick',function(){
+                infowindow.setMarker = null;
+            });
+        }
+    };
+
+    /*
+    // Need function for processing infor from the dropdown menu
+          // This function allows the user to input a desired travel time, in
+          // minutes, and a travel mode, and a location - and only show the listings
+          // that are within that travel time (via that travel mode) of the location
+          function searchWithinTime() {
+            // Initialize the distance matrix service.
+            var distanceMatrixService = new google.maps.DistanceMatrixService;
+            var address = document.getElementById('search-within-time-text').value;
+            // Check to make sure the place entered isn't blank.
+            if (address == '') {
+              window.alert('You must enter an address.');
+            } else {
+              hideListings();
+              // Use the distance matrix service to calculate the duration of the
+              // routes between all our markers, and the destination address entered
+              // by the user. Then put all the origins into an origin matrix.
+              var origins = [];
+              for (var i = 0; i < markers.length; i++) {
+                origins[i] = markers[i].position;
+              }
+              var destination = address;
+              var mode = document.getElementById('mode').value;
+              // Now that both the origins and destination are defined, get all the
+              // info for the distances between them.
+              distanceMatrixService.getDistanceMatrix({
+                origins: origins,
+                destinations: [destination],
+                travelMode: google.maps.TravelMode[mode],
+                unitSystem: google.maps.UnitSystem.IMPERIAL,
+              }, function(response, status) {
+                if (status !== google.maps.DistanceMatrixStatus.OK) {
+                  window.alert('Error was: ' + status);
+                } else {
+                  displayMarkersWithinTime(response);
+                }
+              });
+            }
+          }
+    */
 }
+
+var mapView = new MapView();
